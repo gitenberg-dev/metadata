a4aa04829f3a0234a720f0fd6cbcf215346ce3cb
eric <eric@hellman.net>
2015-06-16 23:58:00 -0400

    added test

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
+import os
+import unittest
 import yaml
 import json
 import metadata.marc as marc
 from metadata.pandata import Pandata
 import pymarc
 
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'samples/pandata.yaml')
+TESTDATA_MARCFILENAME = os.path.join(os.path.dirname(__file__), 'samples/pandata.marc.xml')
 
-pandata = Pandata("github/local/metadata/samples/pandata.yaml")
-print "loaded"
-print pandata.issued_gutenberg
-record = marc.stub(pandata)
-open("github/local/metadata/samples/pandata.marc.xml","w+").write(pymarc.record_to_xml(record))
+class Yaml2MarcTest(unittest.TestCase):
+    pandata = Pandata(TESTDATA_FILENAME)
+    print "loaded"
+    print pandata.issued_gutenberg
+    record = marc.stub(pandata)
+    open(TESTDATA_MARCFILENAME,"w+").write(pymarc.record_to_xml(record))
+
