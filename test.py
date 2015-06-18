751b51d088a4d601febc71afac704ea16673a6d0
eric <eric@hellman.net>
2015-06-18 16:50:33 -0400

    ummm, real tests are better

--- test.py
+++ test.py
@@ -10,9 +10,20 @@ TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'samples/pandata.yam
 TESTDATA_MARCFILENAME = os.path.join(os.path.dirname(__file__), 'samples/testoutput.marc.xml')
 
 class Yaml2MarcTest(unittest.TestCase):
-    pandata = Pandata(TESTDATA_FILENAME)
-    print "loaded"
-    print pandata.issued_gutenberg
-    record = marc.stub(pandata)
-    open(TESTDATA_MARCFILENAME,"w+").write(pymarc.record_to_xml(record))
+    def setUp(self):
+        self.pandata = Pandata(TESTDATA_FILENAME)
+        
+    def test_pandata(self):
+        self.assertTrue( self.pandata.issued_gutenberg == "2007-03-03")
+        self.assertTrue( isinstance( self.pandata.authors , list))
+        self.assertTrue( isinstance( self.pandata.subjects[0] , tuple ))
+        self.assertEqual( self.pandata.subjects[0][0] , u'lcsh' )
+
+    def test_marc(self):
+        record = marc.stub(self.pandata)
+        for field in record.get_fields('650'):
+            
+            self.assertEqual(field.get_subfields('a')[0],  'Science fiction')
+            break
+        open(TESTDATA_MARCFILENAME,"w+").write(pymarc.record_to_xml(record))
 
