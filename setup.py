9629422780d597e9ce99ed451bde0bb655ebabd4
Seth Woodworth <seth@sethish.com>
2015-07-25 17:10:24 -0400

    bumping version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='gitenberg.metadata',
-    version='0.1.4',
+    version='0.1.5',
     description="metadata development - formats and machinery for GITenberg",
     long_description=readme + '\n\n' + history,
     author="Eric Hellman",
