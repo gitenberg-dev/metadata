c84964879208a5a3f8e9e35b2b71a2477a6a8905
Seth Woodworth <seth@sethish.com>
2015-07-26 02:25:40 -0400

    removing 2.6 because we use dict comprehensions

--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ setup(
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         # 'Programming Language :: Python :: 3',
         # 'Programming Language :: Python :: 3.3',
