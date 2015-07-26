e662af1de18598c12db05c44416fa7db3e7ac981
Seth Woodworth <seth@sethish.com>
2015-07-26 02:21:01 -0400

    commenting out py3 support until tests pass

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='gitenberg.metadata',
-    version='0.1.5',
+    version='0.1.6',
     description="metadata development - formats and machinery for GITenberg",
     long_description=readme + '\n\n' + history,
     author="Eric Hellman",
@@ -56,9 +56,9 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
     tests_require=test_requirements
