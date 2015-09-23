407de232e72cb835cbc649a77fa49789c3e3836c
eshellman <eric@hellman.net>
2015-09-23 18:12:08 -0400

    bump version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='gitenberg.metadata',
-    version='0.1.7',
+    version='0.1.8',
     description="metadata development - formats and machinery for GITenberg",
     long_description=readme + '\n\n' + history,
     author="Eric Hellman",
